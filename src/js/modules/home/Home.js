c50f3441043e01340d4ab333af8e6dc4724f8eed
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-11-21 17:27:40 +0200

    home images

--- src/js/modules/home/Home.js
+++ src/js/modules/home/Home.js
@@ -3,13 +3,28 @@ import {Link} from 'react-router'
 
 require('./styles.scss')
 
+const images = [
+  require('./images/laikes_general_image.jpg'),
+  require('./images/astikes_general_image.jpg')
+]
+
 const Home = ()=> (
   <div className="home page">
     <Link to="puzzle" className="left-panel">
-      <h1>Λαικές κατοικίες</h1>
+      <div className="bg" style={{backgroundImage:`url(${images[0]})`}}/>
+      <div className="title">
+        <h1>Λαϊκές</h1>
+        <hr/>
+        <h5>κατοικίες</h5>
+      </div>
     </Link>
     <Link to="puzzle" className="right-panel">
-      <h1>Αστικές κατοικίες</h1>
+      <div className="bg" style={{backgroundImage:`url(${images[1]})`}}/>
+      <div className="title">
+        <h1>Αστικές</h1>
+        <hr/>
+        <h5>κατοικίες</h5>
+      </div>
     </Link>
   </div>
 )
