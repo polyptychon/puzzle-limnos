faebc81b132c5db546c0efe7ebb0105138f24b7d
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-11-23 12:16:12 +0200

    clean up

--- src/js/modules/puzzle/component/Puzzle.js
+++ src/js/modules/puzzle/component/Puzzle.js
@@ -1,5 +1,6 @@
 import React, {Component,PropTypes} from 'react'
 import {connect} from 'react-redux'
+import {Link} from 'react-router'
 import {checkPuzzlePartPosition, initAndShufflePuzzle} from '../actions'
 import {getStyles, getBgImageStyles, getSizeStyles} from './styles'
 
@@ -18,20 +19,29 @@ const images = {
 export class Puzzle extends Component {
   constructor(props) {
     super(props);
-    props.shufflePuzzle()
+  }
+  componentWillMount() {
+    this.props.shufflePuzzle()
   }
   getImage() {
     const {location} = this.props
-    const category = location.pathname.split('/')[1]
+    const paths = location.pathname.split('/')
+    const path = paths.slice(0,paths.length-1).join('/')
+    const category = paths[1]
     let index = this.props.params.index || 1
     if (index>images[category].length){
       index = 1
     }
-    return images[category][index-1]
+    return {
+      images: images[category],
+      image: images[category][index-1],
+      index: parseInt(index),
+      path
+    }
   }
   render() {
-    const {parts, partClick, isPuzzleSolved, shufflePuzzle} = this.props
-    const image = this.getImage()
+    const {parts, partClick, isPuzzleSolved} = this.props
+    const {image,index,path,images} = this.getImage()
     return (
       <div className="game page">
         <div className={isPuzzleSolved?'solved':''}>
@@ -46,7 +56,16 @@ export class Puzzle extends Component {
               ))
             }
           </ul>
-          <a href="javascript:" onClick={()=> shufflePuzzle()} className="restart-puzzle">Play again</a>
+          <div className="puzzle-complete">
+            <div className="puzzle-complete-message">
+              <h2>Συγχαρητήρια τα κατάφερες!</h2>
+              <h3>Να δούμε πως θα τα πας με τα πιο δύσκολα</h3>
+            </div>
+            <div className="puzzle-complete-group">
+              <Link to="/" className="btn puzzle-go-home">Επιστροφή στην αρχική</Link>
+              {index<images.length && <Link to={path+'/'+(index+1)} className="btn puzzle-next">Συνέχισε στην επόμενη</Link>}
+            </div>
+          </div>
         </div>
       </div>
     )
