4b300fffb7efcc2bcf0e9fa1b4ee6703941e363f
harris sidiropoulos <harris.sidiropoulos@gmail.com>
2016-11-25 13:47:51 +0200

    change background

--- src/js/modules/dummy-page/DummyPage.js
+++ src/js/modules/dummy-page/DummyPage.js
@@ -1,3 +1,13 @@
-import React from 'react'
-const DummyPage = () => (<div></div>)
+import React, {Component} from 'react'
+
+class DummyPage extends Component {
+  componentWillMount() {
+    const body = document.querySelector('body')
+    body.classList.remove(`laikes-bg`)
+    body.classList.remove(`astikes-bg`)
+  }
+  render() {
+    return (<div></div>)
+  }
+}
 export default DummyPage
--- src/js/modules/page/Page.js
+++ src/js/modules/page/Page.js
@@ -1,20 +1,27 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router'
 const {data} = require('../data')
 require('./styles.scss')
 
-const Page = ({params}) => {
-  const {lang, category} = params
-  const description = data[lang][category].description
-  return (
-    <div className="content">
-      <div className="content-image">
-        <img src={data[lang][category].katopsi} />
+class Page extends Component {
+  componentWillMount() {
+    const {category} = this.props.params
+    const body = document.querySelector('body')
+    body.classList.add(`${category}-bg`)
+  }
+  render() {
+    const {lang, category} = this.props.params
+    const description = data[lang][category].description
+    return (
+      <div className="content">
+        <div className="content-image">
+          <img src={data[lang][category].katopsi} />
+        </div>
+        <div className="content-text" dangerouslySetInnerHTML={{__html: description}}></div>
+        <Link to={`/${lang}`} className="btn back">{data[lang]['button-puzzle-home-label']}</Link>
       </div>
-      <div className="content-text" dangerouslySetInnerHTML={{__html: description}}></div>
-      <Link to={`/${lang}`} className="btn back">{data[lang]['button-puzzle-home-label']}</Link>
-    </div>
-  )
+    )
+  }
 }
 
 export default Page
